1a70a0f7fd38a099cc95ab973ec1bfd085143963
Guillaume <65726796+0xGuillaume@users.noreply.github.com>
2023-05-08 16:42:55 +0200

    Added inline policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,48 +11,63 @@ terraform {
 provider "aws" {}
 
 
-#data "aws_iam_policy_document" "assume_role" {
-#  statement {
-#    effect = "Allow"
-#
-#    principals {
-#      type        = "Service"
-#      identifiers = ["lambda.amazonaws.com"]
-#    }
-#
-#    actions = ["sts:AssumeRole"]
-#  }
-#}
-#
-#
-#resource "aws_iam_role" "iam_for_lambda" {
-#  name               = "apirole"
-#  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-#}
-#
-#
-#data "archive_file" "lambda" {
-#  type        = "zip"
-#  source_file = "../scripts/lambda_function.py"
-#  output_path = "../scripts/lambda_function_payload.zip"
-#}
-#
-#
-#resource "aws_lambda_function" "test_lambda" {
-#  # If the file is not in the current working directory you will need to include a
-#  # path.module in the filename.
-#  filename      = "../scripts/lambda_function_payload.zip"
-#  function_name = "ApiScan"
-#  role          = aws_iam_role.iam_for_lambda.arn
-#  handler       = "index.test"
-#
-#  source_code_hash = data.archive_file.lambda.output_base64sha256
-#
-#  runtime = "python3.10"
-#
-#  environment {
-#    variables = {
-#      foo = "bar"
-#    }
-#  }
-#}
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "assets-apirole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  
+  inline_policy {
+    name = "DynamoDbScan"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = ["dynamodb:Scan"]
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]
+    })
+  }
+}
+
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "../scripts/lambda_function.py"
+  output_path = "../scripts/lambda_function_payload.zip"
+}
+
+
+resource "aws_lambda_function" "lambda" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "../scripts/lambda_function_payload.zip"
+  function_name = "ApiScanTf"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "lambda_function.lambda_handler"
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  runtime = "python3.10"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
