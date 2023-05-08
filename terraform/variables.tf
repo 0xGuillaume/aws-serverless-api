0f3e9bb02d48213c810ce5f971714d3714e872f3
Guillaume <65726796+0xGuillaume@users.noreply.github.com>
2023-05-08 22:08:42 +0200

    Added tf variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,44 @@
 variable "ddb_table" {
-  type = string
+  type    = string
   default = "assets"
 }
 
-
-variable "ddb_hash_key" {
-  type = string
+variable "ddb_hashkey" {
+  type    = string
   default = "hostname"
+}
+
+variable "ddb_policy" {
+  type    = string
+  default = "DynamoDBScanAllow"
+}
+
+variable "ddb_table" {
+  type    = string
+  default = "assets"
+}
+
+variable "lambda_role" {
+  type    = string
+  default = "api-lambda-role"
+}
+
+variable "apigw_name" {
+  type    = string
+  default = "api-assets-serverless"
+}
+
+variable "apigw_pathpart" {
+  type    = string
+  default = "assets"
+}
+
+variable "apigw_stage" {
+  type    = string
+  default = "dev"
+}
+
+variable "lambda_function" {
+  type    = string
+  default = "api-assets-scan"
 }
