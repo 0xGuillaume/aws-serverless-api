8539945dddc6a8a1aecd9c9d5e80eebf46694e3d
Guillaume <65726796+0xGuillaume@users.noreply.github.com>
2023-05-08 16:44:27 +0200

    Removed lambda code

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,28 +46,3 @@ resource "aws_iam_role" "iam_for_lambda" {
 }
 
 
-data "archive_file" "lambda" {
-  type        = "zip"
-  source_file = "../scripts/lambda_function.py"
-  output_path = "../scripts/lambda_function_payload.zip"
-}
-
-
-resource "aws_lambda_function" "lambda" {
-  # If the file is not in the current working directory you will need to include a
-  # path.module in the filename.
-  filename      = "../scripts/lambda_function_payload.zip"
-  function_name = "ApiScanTf"
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "lambda_function.lambda_handler"
-
-  source_code_hash = data.archive_file.lambda.output_base64sha256
-
-  runtime = "python3.10"
-
-  environment {
-    variables = {
-      foo = "bar"
-    }
-  }
-}
