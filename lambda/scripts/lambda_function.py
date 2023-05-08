002a9cc81374654704e21f73c7aa7f0acd7a61bd
Guillaume <65726796+0xGuillaume@users.noreply.github.com>
2023-05-08 22:36:57 +0200

    Added lambda functions code

---
+++ lambda/scripts/lambda_function.py
@@ -0,0 +1,24 @@
+import boto3
+import json
+
+
+client = boto3.client('dynamodb')
+
+
+def lambda_handler(context, event):
+    """."""
+
+    data = client.scan(
+        TableName="assets-tf"
+    )
+
+    response = {
+      'statusCode': 200,
+      'body': json.dumps(data["Items"]),
+      'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+    }
+
+    return response
