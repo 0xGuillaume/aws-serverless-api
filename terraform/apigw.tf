81593bcaa0ef07db31a30dbc11f95629f4486252
Guillaume <65726796+0xGuillaume@users.noreply.github.com>
2023-05-08 16:43:09 +0200

    Added API GW V1

--- terraform/apigw.tf
+++ terraform/apigw.tf
@@ -1,10 +1,39 @@
-resource "aws_apigatewayv2_api" "example" {
-  name                       = "example-websocket-api"
-  protocol_type              = "HTTP"
-  #route_selection_expression = "${request.method}"
+resource "aws_api_gateway_rest_api" "api" {
+  name        = "serverless-api"
+  description = "This is a serverless API for demonstration purposes."
 }
 
-resource "aws_apigatewayv2_route" "example" {
-  api_id    = aws_apigatewayv2_api.example.id
-  route_key = "ANY /assets/"
+resource "aws_api_gateway_resource" "api" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "assets"
+}
+
+resource "aws_api_gateway_method" "api" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.api.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+
+resource "aws_api_gateway_integration" "api" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.api.id
+  http_method             = aws_api_gateway_method.api.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda.invoke_arn
+}
+
+
+resource "aws_lambda_permission" "api" {
+  statement_id  = "AllowMyDemoAPIInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The /* part allows invocation from any stage, method and resource path
+  # within API Gateway.
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*"
 }
