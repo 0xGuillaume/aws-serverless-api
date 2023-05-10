c1ecc4c13829caa71deec8e3ca8b2620b8ed7865
0xGuillaume <65726796+0xGuillaume@users.noreply.github.com>
2023-05-10 17:17:29 +0200

    Added dict variable to handle apigw routes and lambdas creation

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -33,7 +33,49 @@ variable "apigw_stage" {
   default = "dev"
 }
 
+variable "apigw" {
+  type = list(object({
+    name = string
+    path = string
+  }))
+  default = [
+    {
+      "name": "asset-scan",
+      "path": "assets"
+    },
+    {
+      "name": "asset-get-item",
+      "path": "asset"
+    }
+  ]
+}
+
+variable "lambda" {
+  type = list(object({
+    name = string
+    runtime = string
+    handler = string
+  }))
+  default = [
+    {
+      "name": "asset-scan",
+      "runtime": "python3.10",
+      "handler": "lambda_function.scan"
+    },
+    {
+      "name": "asset-get-item",
+      "runtime": "python3.10",
+      "handler": "lambda_function.get_item"
+    }
+  ]
+}
+
 variable "lambda_function" {
   type    = string
   default = "api-assets-scan"
+}
+
+variable "lambda_function_get_item" {
+  type    = string
+  default = "api-assets-get-item"
 }
