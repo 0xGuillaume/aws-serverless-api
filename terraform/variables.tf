c1c3d2f34f33a5d25d13862c19944ef2ebdb8384
0xGuillaume <65726796+0xGuillaume@users.noreply.github.com>
2023-05-11 21:00:24 +0200

    Added new lambda object

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -38,34 +38,48 @@ variable "apigw" {
     name = string
     path = string
   }))
+
   default = [
     {
-      "name": "asset-scan",
-      "path": "assets"
+      "name" : "asset-scan",
+      "path" : "assets"
     },
     {
-      "name": "asset-get-item",
-      "path": "asset"
+      "name" : "asset-get-item",
+      "path" : "asset"
     }
   ]
 }
 
 variable "lambda" {
+  type = object({
+    source_file = string
+    output_file = string
+  })
+
+  default = {
+    "source_file": "../lambda/parser.py",
+    "output_file": "../lambda/lambda_function_payload.zip"
+  }
+}
+
+variable "lambda_handlers" {
   type = list(object({
-    name = string
+    name    = string
     runtime = string
     handler = string
   }))
+
   default = [
     {
-      "name": "asset-scan",
-      "runtime": "python3.10",
-      "handler": "lambda_function.scan"
+      "name" : "asset-scan",
+      "runtime" : "python3.10",
+      "handler" : "parser.scan"
     },
     {
-      "name": "asset-get-item",
-      "runtime": "python3.10",
-      "handler": "lambda_function.get_item"
+      "name" : "asset-get-item",
+      "runtime" : "python3.10",
+      "handler" : "parser.get_item"
     }
   ]
 }
