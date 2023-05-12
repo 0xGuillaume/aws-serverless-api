a2765b7abbee6a06f2e4cd9d9aed7c04bf48803e
Guillaume <65726796+0xGuillaume@users.noreply.github.com>
2023-05-12 23:18:43 +0200

    Added outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,15 @@
 output "api_url" {
   value = aws_api_gateway_deployment.api.invoke_url
 }
+
+output "stage" {
+  value = var.apigw.stage
+}
+
+output "uri_scan" {
+  value = var.apigw_methods[0].path
+}
+
+output "uri_put_item" {
+  value = var.apigw_methods[1].path
+}
