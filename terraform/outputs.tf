d41f6f8e3f2b935233026f111c09d89b52f311dd
Guillaume <65726796+0xGuillaume@users.noreply.github.com>
2023-05-13 14:56:41 +0200

    Added outputs description

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,22 @@
 output "api_url" {
-  value = aws_api_gateway_deployment.api.invoke_url
+  value       = aws_api_gateway_deployment.api.invoke_url
+  description = "The ApiGateway url."
 }
+
 
 output "stage" {
-  value = var.apigw.stage
+  value       = var.apigw.stage
+  description = "The ApiGateway stage name."
 }
+
 
 output "uri_scan" {
-  value = var.apigw_methods[0].path
+  value       = var.apigw_methods[0].path
+  description = "URI of the scan methods."
 }
 
+
 output "uri_put_item" {
-  value = var.apigw_methods[1].path
+  value       = var.apigw_methods[1].path
+  description = "URI of the get_item methods."
 }
