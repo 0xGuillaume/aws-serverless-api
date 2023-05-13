5b876ae68f46a9f0bbda4345452dbe7486e71976
0xGuillaume <65726796+0xGuillaume@users.noreply.github.com>
2023-05-13 11:55:25 +0200

    Added formatted output

--- demo.py
+++ demo.py
@@ -1,4 +1,6 @@
 """Module to query apigw items."""
+from colorama import Fore
+from random import choice
 import subprocess
 from time import sleep
 import json
@@ -28,8 +30,8 @@ class Demo:
         self.outputs = self._outputs()
         self.url = self._url()
 
-        self._requests(self._scan())
-        print(self._get_item())
+        self._run()
+
 
     def _outputs(self) -> dict:
         """Get terraform outputs."""
@@ -48,26 +50,65 @@ class Demo:
         stage       = self.outputs["stage"]["value"]
 
         return f"{base_url}{stage}/"
+    
+
+    def _output(self, code:str, uri:str) -> None:
+        """Formats requests output.
+
+        Arguments:
+            code: HTTP status code.
+            uri: URI's name requested.
+        """
+
+        code = str(code)
+        output = f"[{code}] - HTTP /GET - {uri.upper()}"
+
+        if code.startswith("2"):
+            return Fore.CYAN + output + Fore.RESET
+
+        else:
+            return Fore.RED + output + Fore.RESET
+
+
+    def _requests(self, url:str) -> None:
+        """Run HTTP request.
+
+        Argument:
+            url: HTTP url to request.
+        """
+
+        return requests.get(url).status_code
 
 
     def _scan(self) -> str:
         """Returns ApiGateway scan url."""
 
-        return self.url + self.outputs["uri_scan"]["value"]
+        url = self.url + self.outputs["uri_scan"]["value"]
+        code = self._requests(url)
+
+        return print(self._output(code, "scan"))
 
 
     def _get_item(self) -> str:
         """Returns ApiGateway get_item url."""
 
-        return self.url + self.outputs["uri_put_item"]["value"]
+        url = self.url + self.outputs["uri_put_item"]["value"]
+        code = self._requests(url)
+
+        return print(self._output(code, "get item"))
 
 
-    def _requests(self, url:str) -> None:
-        """."""
+    def _run(self) -> None:
+        """Run X HTTP requests for demo purpose."""
 
-        return requests.get(url).status_code
+        for index in range(1, self.amount + 1):
 
-
+            choice([self._scan(), self._get_item()])
+    
+            #self._get_item()
+            #self._scan()
+            sleep(1)
+            
 
 if __name__ == "__main__":
     Demo(args.amount)
