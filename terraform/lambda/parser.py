e7e88ee32d1ca97a296bad1fdd39418ea6c541d5
0xGuillaume <65726796+0xGuillaume@users.noreply.github.com>
2023-05-16 23:29:12 +0200

    Added lambda code to tf dir

---
+++ terraform/lambda/parser.py
@@ -0,0 +1,49 @@
+"""Functions to handle DynamoDB table."""
+import json
+from boto3 import client
+
+
+client = client('dynamodb')
+
+
+def scan(context:dict, event) -> dict:
+    """Get all items of DynamoDB asset table."""
+
+    data = client.scan(
+        TableName="assets"
+    )
+
+    response = {
+      'statusCode': 200,
+      'body': json.dumps(data["Items"]),
+      'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+    }
+
+    return response
+
+
+def get_item(context:dict, event) -> dict:
+    """Get a single item of DynamoDb asset table."""
+
+    hostname = context["pathParameters"]["hostname"]
+
+    item = client.get_item(
+        TableName="assets",
+        Key={
+            "hostname": {"S": hostname}
+        }
+    )
+
+    response = {
+      'statusCode': 200,
+      'body': json.dumps(item["Item"]),
+      'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+    }
+
+    return response
