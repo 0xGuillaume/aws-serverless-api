c5a54c3a44f2109f73000ec3c35eadded07a9344
0xGuillaume <65726796+0xGuillaume@users.noreply.github.com>
2023-05-18 14:47:12 +0200

    Added role description

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -13,6 +13,7 @@ data "aws_iam_policy_document" "assume_role" {
 
 
 resource "aws_iam_role" "iam_for_lambda" {
+  description        = "Allows lamba to perform Scan & Get-Item actions on DummyAssetsDDB."
   name               = var.lambda.role
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 
